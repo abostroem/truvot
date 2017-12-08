4550fef91440dd471e61e6b853398e6e8b03fe2b
Azalee Bostroem <abostroem@gmail.com>
2017-12-08 03:28:14 +0000

    modifying script so it works on my computer with UVOTPY

--- example/uvotpy_script.py
+++ example/uvotpy_script.py
@@ -3,33 +3,37 @@
 
 #import some python packages
 import os
-import uvotgrism
+from uvotpy import uvotgrism
 from astropy.io import fits
+import numpy as np
+
+
 
 ra, dec = 210.7738750, +54.2736389  #SET VARIABLES REQUIRED FOR UVOTPY (TARGET LOCATION ON SKY)
-
+calfile = '/dark/bostroem/research/not_my_code/uvotpy/calfiles/swugu0160wcal20041120v002.fits'
+fluxcalfile = '/dark/bostroem/research/not_my_code/uvotpy/calfiles/swugu0160_20041120v105.arf'
 os.chdir('00032094004/uvot/image') #CHANGE WORKING DIRECTORY TO BE THE DATA DIRECTORY
 
 obsid = '00032094004' #OBSID OF OBSERVATION
 ext = 1 #OPERATE ON THE 1ST IMAGE EXTENSION
 e=fits.getdata('sw00032094004ugu_dt_template_1.fits') #READ IN THE TEMPLATE FOR THIS SNAPSHOT AS AN ARRAY
-Z = uvotgrism.getSpec(ra,dec,obsid,ext,fit_second=True,wr_outfile=True,chatter=1,background_template=e) #EXECUTE UVOTPY
+Z = uvotgrism.getSpec(ra,dec,obsid,ext,fit_second=True,wr_outfile=True,chatter=1,calfile=calfile,background_template=e) #EXECUTE UVOTPY
 ext = 2 #REPEAT THE PROCEDURE FOR THE 2ND IMAGE EXTENSION
 e=fits.getdata('sw00032094004ugu_dt_template_2.fits')
-Z = uvotgrism.getSpec(ra,dec,obsid,ext,fit_second=True,wr_outfile=True,chatter=1,background_template=e)
+Z = uvotgrism.getSpec(ra,dec,obsid,ext,fit_second=True,wr_outfile=True,chatter=1,calfile=calfile,background_template=e)
 ext = 3 #REPEAT THE PROCEDURE FOR THE 3RD IMAGE EXTENSION
 e=fits.getdata('sw00032094004ugu_dt_template_3.fits')
-Z = uvotgrism.getSpec(ra,dec,obsid,ext,fit_second=True,wr_outfile=True,chatter=1,background_template=e)
+Z = uvotgrism.getSpec(ra,dec,obsid,ext,fit_second=True,wr_outfile=True,chatter=1,calfile=calfile,background_template=e)
 os.chdir('../../..') #CHANGE BACK TO THE ORIGINAL DIRECTORY
 print 'Finished extracting using UVOTPY!'
 
 #CONVERT THE UVOTPY OUTPUT SPECTRA INTO A COLUMNATED FORMAT
 thr = fits.open('00032094004/uvot/image/sw00032094004ugu_1ord_1_f.pha')
 data = thr[2].data                                                   
-numpy.savetxt('00032094004/uvot/image/sw00032094004ugu_1ord_1.dat',data)
+np.savetxt('00032094004/uvot/image/sw00032094004ugu_1ord_1.dat',data)
 thr = fits.open('00032094004/uvot/image/sw00032094004ugu_1ord_2_f.pha')
 data = thr[2].data                                                   
-numpy.savetxt('00032094004/uvot/image/sw00032094004ugu_1ord_2.dat',data)
+np.savetxt('00032094004/uvot/image/sw00032094004ugu_1ord_2.dat',data)
 thr = fits.open('00032094004/uvot/image/sw00032094004ugu_1ord_3_f.pha')
 data = thr[2].data                                                   
-numpy.savetxt('00032094004/uvot/image/sw00032094004ugu_1ord_3.dat',data)
+np.savetxt('00032094004/uvot/image/sw00032094004ugu_1ord_3.dat',data)
